4a1271efb8a5a60df4421ff87e5e44db178ac816
PixelPioneer2003 <codinglit2908@gmail.com>
2024-12-19 11:04:18 +0530

    Bcryptjs is added

--- Backend/controllers/Auth.js
+++ Backend/controllers/Auth.js
@@ -1,6 +1,6 @@
 const User = require("../models/User");
 const PersonalProfile = require("../models/personalProfile");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const OTP = require("../models/Otp");
